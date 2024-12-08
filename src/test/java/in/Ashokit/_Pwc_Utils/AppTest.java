ea06037cd3dbfe08840894114aa51785058b84f9
amirzubai <mdamir0254@gmail.com>
2024-12-09 00:59:56 +0530

    first commit

---
+++ src/test/java/in/Ashokit/_Pwc_Utils/AppTest.java
@@ -0,0 +1,19 @@
+package in.Ashokit._Pwc_Utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
